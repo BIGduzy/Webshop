126830c3453639074247db04e36c6f98260cc459
Nick <nick-b@live.nl>
2013-04-25 15:18:41 +0200

    De producten worden nu op de homepage getoon en plaatjes

--- application/models/user.php
+++ application/models/user.php
@@ -23,5 +23,38 @@
 		return $this->query($query);
 
 	}
+
+	public function insert_register_data($post)
+	{
+		$query = "INSERT INTO `logins` 
+				  SET `username` = '".$post['emailaddress']."',
+					  `password` = '".$post['password']."'";
+		//echo $query;exit(); //invoeren in sql binnen phpmyadmin
+		$this->query($query);
+		$id = $this->find_last_inserted_id();
+		$query = "INSERT INTO `users` 
+				  SET `user_id` = '".$id."',
+					  `firstname` = '".$post['firstname']."',
+					  `infix` = '".$post['infix']."',
+					  `surname` = '".$post['surname']."',
+					  `street` = '".$post['street']."',
+					  `housenumber` = '".$post['housenumber']."',
+					  `zipcode` = '".$post['zipcode']."',
+					  `residence` = '".$post['residence']."',
+					  `phonenumber` = '".$post['phonenumber']."',
+					  `mobilephonenumber` = '".$post['mobilephonenumber']."'";
+		$this->query($query);
+		$query = "INSERT INTO `userroles` ( `userrole_id`,
+											`role`)
+								VALUES	  ( '".$id."',
+											'customer')";
+		$this->query($query);
+	}
+
+	public function select_all_products($limit=4, $offset=0)
+	{
+		$query = "SELECT * FROM `products` LIMIT ".$limit." OFFSET ".$offset;
+		return $this->query($query);
+	}
  }
 ?>
--- application/views/users/homepage.php
+++ application/views/users/homepage.php
@@ -1,1 +1,55 @@
+<style>
+.odd
+{
+	background-color :RGBA(214,214,214,1);
+    border :1px solid red;
+	font-size:1em;
+}
+
+.even
+{
+	background-color:RGBA(214,214,214,0.5);
+	border:1px solid black;
+	font-size:1em;
+}
+
+.highlight
+{
+	background-color:RGBA(200,200,200,1.0);
+	border:1px solid black;
+	font-size:1em;
+}
+
+td, th
+{
+	padding:0.3em 1.0em;
+	text-align:center;
+}
+
+</style>
+<script type='text/javascript'>
+	$('document').ready(function(){
+		$("tr:even").addClass("even");
+		$("tr:odd").addClass("odd");
+		$("tr").hover(
+			function(){
+				$(this).toggleClass('highlight');
+			},
+			function(){
+				$(this).toggleClass('highlight');
+			}
+		);
+	});
+</script>
+
 <h3><?php echo $header; ?></h3>
+<table>
+	<tr>
+		<th>artikelnr.</th>
+		<th>productfoto</th>
+		<th>productnaam</th>
+		<th>prijs</th>
+	</tr>
+	<?php echo $products; ?>
+</table>
+<?php echo $pagenumbers; ?>
